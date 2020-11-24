b85b584449462815bf116c6d0f2c58af26b46978
guiguketang <guiguketang@163.com>
2020-11-24 11:01:38 +0800

    fix the bug when the log contains "\xac\xed\x00\x05t\" content……

--- doc/cmd/cmdgroup.sh
+++ doc/cmd/cmdgroup.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-grep -v 'OK' /opt/devops/cmd/dbm.log|head -$1|awk '{if($2~/\[0/)print $4 " " $5}'|grep -v '^$'|sort|uniq -c|sort -k1,1nr|head -10
+#grep -v 'OK' /opt/devops/cmd/dbm.log|awk '{print $4 " " $5}'|sort|uniq -c|sort -k1,1nr|head -10
+grep -v 'OK' /opt/devops/cmd/dbm.log|head -$1|awk '{print $4 " " $5}'|grep -v '^$'|sort|uniq -c|sort -k1,1nr|head -10
+#grep -v 'OK' /opt/devops/cmd/dbm.log|head -$1|awk '{if($2~/\[0/)print $4 " " $5}'|grep -v '^$'|sort|uniq -c|sort -k1,1nr|head -10
